ea7950c6064228b67b83822bcf58b7e772650e24
cice <cice@A8POVMac07.local>
2018-07-09 19:18:32 +0200

    Primer commit de proyecto

---
+++ src/com/instancia2/Vehiculo.java
@@ -0,0 +1,17 @@
+package com.instancia2;
+
+/**
+ * Cl se ejemplo de Herencia
+ */
+public class Vehiculo {
+
+    protected String color;
+    protected int potencia;
+
+    public Vehiculo(){
+        super();
+        this.color = "Verde";
+        this.potencia = 50;
+    }
+
+}
